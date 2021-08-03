a53f36ca2d0f3ae8bc5101d31de4c4cc9f5d1914
Fernando Antonio Gonzalez Gaete <fernandogonzalezgaete@gmail.com>
2021-08-03 17:33:38 -0400

    listongo

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,14 @@
 class QuestionsController < ApplicationController
-    def ask
+  def ask
+  end
+
+  def answer
+    if params[:ask] == "I am going to work"
+      @respuesta = 'Great!'
+    elsif params[:ask].end_with?("?")
+      @respuesta = 'Silly question, get dressed and go to work!'
+    else
+      @respuesta = "I don't care, get dressed and go to work!"
     end
+  end
 end
